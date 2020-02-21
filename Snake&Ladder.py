40faa03eb4f37ca73fed3866ed03e6cc123f67cc
UTKARSH <utkarsht724>
2020-02-21 16:32:23 +0530

    Added:Use case 1

--- Snake&Ladder.py
+++ Snake&Ladder.py
@@ -1,13 +1,11 @@
 import random
 def SnakeAndLadder():
-  try:
+
     P1 = True
 
     Snake = {30: 6, 37: 13, 49: 9, 59: 19, 68: 3, 66: 35, 88: 32, 82: 44, 94: 55, 98: 14}
 
     ladder = {2: 21, 17: 36, 11: 48, 39: 80, 31: 71, 53: 92, 74: 93, 65: 84, 62: 83, 86: 97}
-
-    #use case1
 
     Player = {'P1': 0, 'P2': 0}
     DiceCount = 0
