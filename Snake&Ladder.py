641a071cb569001aff3a4e39477b3e016cf32665
UTKARSH <utkarsht724>
2020-02-22 08:58:08 +0530

    ADDED:Use case 3

--- Snake&Ladder.py
+++ Snake&Ladder.py
@@ -11,5 +11,51 @@ def SnakeAndLadder():
     DiceCount = 0
     Position = 0
 
-  dice = random.randint(1, 6)
-  print("Dice Number:", dice)
+    dice = random.randint(1, 6)
+    print("Dice Number:", dice)
+
+    Position = int(Player['P1']) + dice
+
+    if Position in Snake:
+        print("Snake Bite:")
+        Player['P1'] = int(Snake[Position])
+
+    elif Position in ladder:
+        print("get a ladder")
+        Player['P1'] = int(ladder[Position])
+
+    else:
+        if Position <= 100:
+            Player['P1'] = Position
+    DiceCount += 1
+
+    P1 = False
+    P2 = True
+    print("Player 1:your Position is " + str(Player['P1']))
+
+    # FOR PLAYER2
+
+    elif P2:
+    print("Player 2 : now its your chance:")
+    input()
+
+    dice = random.randint(1, 6)
+    print("Dice Numer:", dice)
+    Position = int(Player['P2']) + dice
+    if Position in Snake:
+        print("Snake Bite:")
+        Player['P2'] = int(Snake[Position])
+    elif Position in ladder:
+        print("WOW u get a ladder")
+        Player['P2'] = int(ladder[Position])
+    else:
+        if Position < 100:
+        Player['P2'] = Position
+    DiceCount += 1
+    P1 = True
+    P2 = False
+    print("Player 2: Your Position Is " + str(Player['P2']))
+
+
+
+
